58d45b2ab8897ed9731da61d3723279db53e7442
abhiramisbabu <abhirami@deerom.com>
2021-02-11 11:23:34 +0530

    child class bus creted

--- vehicle.py
+++ vehicle.py
@@ -1,11 +1,9 @@
 class Vehicle:
-    def __init__(self, max_speed, mileage):
+
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
         self.max_speed = max_speed
         self.mileage = mileage
-
-modelX = Vehicle(240, 18)
-print(modelX.max_speed, modelX.mileage)
-
-class Vehicle:
+class Bus(Vehicle):
     pass
 
