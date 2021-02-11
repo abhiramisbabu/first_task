9899150499eb63e6a17bd237176919e256dbd19b
Gopika <gopika@deerom.com>
2021-02-11 12:04:03 +0530

    object created

--- vehicle.py
+++ vehicle.py
@@ -6,4 +6,7 @@ class Vehicle:
         self.mileage = mileage
 class Bus(Vehicle):
     pass
+School_bus = Bus("School Volvo", 180, 12)
+print("Vehicle Name:",School_bus.name,"Speed:", School_bus.max_speed,"Mileage:", School_bus.mileage)
+
 
